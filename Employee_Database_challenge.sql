4e705e457c2adb20f1b98d73131854fd1f2ad408
NAppazeller <75140449+NAppazeller@users.noreply.github.com>
2021-02-10 20:28:40 -0600

    Update Employee_Database_challenge.sql

--- Employee_Database_challenge.sql
+++ Employee_Database_challenge.sql
@@ -1,3 +1,4 @@
+--Deliverable 1: The Number of Retiring Employees by Title
 --Create new table for retirement titles PART 1A
 SELECT e.emp_no,
     e.first_name,
@@ -23,6 +24,21 @@ ORDER BY rt.emp_no, to_date DESC;
 
 -- Employee count by department number PART 1C
 SELECT COUNT(ut.title), ut.title
+INTO retiring_titles
 FROM unique_titles as ut
 GROUP BY ut.title
 ORDER BY count(ut.title) DESC;
+
+--Deliverable 2: The Employees Eligible for the Mentorship Program
+SELECT e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	t.from_date,
+	t.to_date,
+	t.title
+INTO mentorship_eligibility
+FROM employees as e LEFT JOIN titles as t
+ON (t.emp_no = e.emp_no)
+WHERE (t.to_date = '9999-01-01')
+	AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31');
